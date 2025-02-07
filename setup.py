392907a29de73d1267b24cea27587c26d4b9b316
Smarandii <olegsmarandi@gmail.com>
2025-02-07 21:22:09 +0300

    rubank-api-client | SberBankApiClient | v0.0.1

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rubank_api_client",
+    version="0.0.1",
+    description="A Python client for interacting with Sber Bank's online services.",
+    author="Smarandii",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "selenium",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
